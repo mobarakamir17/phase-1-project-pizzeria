a36d4c8c4c9cbef4a8eecee93b51e8e6cae343a4
Amir Mobarak <mobarakamir17@gmail.com>
2023-06-29 12:10:48 -0400

    stars event listener

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let currentPizza;
 let cartItemsMap = {}; // Map to track the quantity of each item in the cart
 let currentRunningTotal = 0; // Use to display total $ amount on screen?  Still working on this, see updateTotal function
+let selectedRating = 0; // Variable to store the selected rating
 
 // Left hand side list of pizza pictures
 const pizzaList = document.getElementById("pizza-list");
@@ -17,22 +18,26 @@ const removeButton = document.getElementById("remove");
 // Cart elements
 const cartList = document.getElementById("cart-list")
 const clearCartButton = document.getElementById("clear-cart")
-const placeOrderButton = document.querySelector("#place-order")
+const placeOrderButton = document.getElementById("place-order")
 const cartItems = document.getElementById("cart-area").querySelector("ul");
 const totalElement = document.getElementById("total");
 // Modal elements
-const popupModal = document.getElementById("popup-modal")
+const popupModal = document.getElementById("popup-modal");
 const closeModalButton = document.querySelector(".close")
+const ratingContainer = document.getElementById("rating-container");
+const ratingStars = document.querySelectorAll(".rating-star");
+const submitRatingButton = document.getElementById("submit-rating-button");
 // Filter elements
 const pizzaAll = document.getElementById("all")
 const pizzaClassic = document.getElementById("classic")
 const pizzaPopular = document.getElementById("popular")
 const pizzaNew = document.getElementById("new")
+const allFilterButtons = document.getElementsByClassName("nav-link")
 // New Pizza Form Elements
 const createNewPizzaForm = document.getElementById("create-new")
 const newName = document.getElementById("new-name")
 const newIng1 = document.getElementById("ingredient1")
-const newIng2 = document.getElementById("ingredient2")
+const newIng2 = document.getElementById("pepperoni")
 const newIng3 = document.getElementById("ingredient3")
 const newIng4 = document.getElementById("ingredient4")
 const newIng5 = document.getElementById("ingredient5")
@@ -42,8 +47,13 @@ const newIng8 = document.getElementById("ingredient8")
 const newIng9 = document.getElementById("ingredient9")
 const newRating = document.getElementById("new-rating")
 const newRequest = document.getElementById("new-request")
-const customPizzaBase = document.getElementById("detail-area")
+const customPizzaBase = document.getElementById("pic-detail-area")
+//^ want this to append to top of detail area so we can see custom creation
 
+const testGetToppingList = document.querySelectorAll(".toppingsIng")
+const testToppingList = Array.from(testGetToppingList)
+// console.log(testToppingList)
+// testToppingList.forEach(topping => console.log(topping.value))
 
 fetch("http://localhost:3000/pizzas")
 .then(resp => resp.json())
@@ -55,10 +65,8 @@ fetch("http://localhost:3000/pizzas")
   clearCart();
   createCustomPizza();
   displayPizzaToppings()
-  displayPizzaClassics(data);
-  displayPizzaPopulars(data);
-  displayPizzaNews(data);
-  displayPizzaAll(data);
+  displayPizzaFilter(data);
+  changeFilterColors();
 })
 
 
@@ -76,13 +84,18 @@ function displayPizzaDetails(){
 
 
 function addPizzaToList(pizza){
+    const pizzaName = document.createElement("div")
     const img = document.createElement("img")
     img.src = pizza.image
-    pizzaList.append(img)
-  
-    const pizzaName = document.createElement("div")
+    img.id = "pizza-image";
+    
     pizzaName.textContent = pizza.name
+    
+    pizzaName.insertBefore(img, pizzaName.firstChild)
+  
     pizzaList.append(pizzaName)
+
+
     
     //event listener will show the selected pizza's details in the central detail section of the screen
     img.addEventListener("click", () => {
@@ -190,6 +203,7 @@ function updateTotal(addOrSub) {
 function createCustomPizza(){
   customPizzaImage.addEventListener("click", () => {
     createNewPizzaForm.style.display = "block"
+    
     // Reset the pizza details
     const headers = document.getElementById("hideable-area");
     headers.style.display = "none";
@@ -197,6 +211,7 @@ function createCustomPizza(){
   });
   createNewPizzaForm.addEventListener("submit", (e) => {
     e.preventDefault();
+
     const newPizza = {
      "name": newName.value,
       "type": "Custom",
@@ -243,17 +258,56 @@ function toggleDetails() {
   });
 }
 // Add an event listener to the image
-currentPizzaImage.addEventListener("click", function() {
-  // Toggle the display property of the headers
-  headers.forEach(function(header) {
-    if (header.style.display === "none") {
-      header.style.display = "block";
-    } else {
-      header.style.display = "none";
+// currentPizzaImage.addEventListener("click", function() {
+//   // Toggle the display property of the headers
+//   headers.forEach(function(header) {
+//     if (header.style.display === "none") {
+//       header.style.display = "block";
+//     } else {
+//       header.style.display = "none";
+//     }
+//   });
+// });
+
+ratingContainer.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("rating-star")) {
+      const starIndex = Array.from(ratingStars).indexOf(event.target) + 1;
+      highlightStars(starIndex);
     }
   });
-});
-
+  
+  ratingContainer.addEventListener("mouseout", () => {
+    highlightStars(selectedRating);
+  });
+  
+  ratingContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("rating-star")) {
+      const starIndex = Array.from(ratingStars).indexOf(event.target) + 1;
+      selectedRating = starIndex;
+      highlightStars(starIndex);
+    }
+  });
+  
+  submitRatingButton.addEventListener("click", () => {
+    // Process the selected rating
+    console.log("Selected Rating:", selectedRating);
+  
+    // Clear the rating
+    selectedRating = 0;
+  
+    // Refresh the page
+    location.reload();
+  });
+  
+  function highlightStars(starIndex) {
+    ratingStars.forEach((star, index) => {
+      if (index < starIndex) {
+        star.classList.add("filled");
+      } else {
+        star.classList.remove("filled");
+      }
+    });
+  }
 
 placeOrderButton.addEventListener("click", () => {
   // Clear the cart contents
@@ -300,19 +354,31 @@ removeButton.addEventListener("click", () => {
 
 function displayPizzaToppings() {
   // show topping pic when checkbox clicked
-  let toppingPic = document.createElement('img')
-  toppingPic.src = "/images/pepperoni pizza.png"
+  let toppingPicPepp = document.createElement('img')
+  toppingPicPepp.src = "/images/pepperoni top.PNG"
   newIng2.addEventListener("click", (e) => {
     if (newIng2.checked) {
-      customPizzaBase.append(toppingPic)
+      customPizzaBase.append(toppingPicPepp)
     } else {
-        toppingPic.remove()
+        toppingPicPepp.remove()
     }
   })
 }
 
 
-function displayPizzaClassics(pizzaData) {
+
+
+
+// const testGetToppingList = document.querySelectorAll(".toppingsIng")
+// const testToppingList = Array.from(testGetToppingList)
+// console.log(testToppingList)
+// testToppingList.forEach(topping => console.log(topping.value))
+
+
+
+
+
+function displayPizzaFilter(pizzaData) {
   pizzaClassic.addEventListener("click", () => {
     // innerhtml resets instead of adding data
     pizzaList.innerHTML = "";
@@ -322,10 +388,6 @@ function displayPizzaClassics(pizzaData) {
       }
     })
   })
-}
-
-
-function displayPizzaPopulars(pizzaData) {
   pizzaPopular.addEventListener("click", () => {
     pizzaList.innerHTML = "";
     pizzaData.forEach(pizza => {
@@ -334,10 +396,6 @@ function displayPizzaPopulars(pizzaData) {
       }
     })
   })
-}
-
-
-function displayPizzaNews(pizzaData) {
   pizzaNew.addEventListener("click", () => {
     pizzaList.innerHTML = "";
     pizzaData.forEach(pizza => {
@@ -346,14 +404,38 @@ function displayPizzaNews(pizzaData) {
       }
     })
   })
-}
-    
-
-function displayPizzaAll(pizzaData) {
   pizzaAll.addEventListener("click", () => {
     pizzaList.innerHTML = ""
     pizzaData.forEach(pizza => {
       addPizzaToList(pizza);
     })
   })
-};
+}
+
+
+function changeFilterColors() {
+  pizzaAll.addEventListener("mouseenter", () => {
+    pizzaAll.style.color = "red";
+  })
+  pizzaAll.addEventListener("mouseout", ()=> {
+    pizzaAll.style.color = "white";
+  })
+  pizzaClassic.addEventListener("mouseenter", () => {
+    pizzaClassic.style.color = "red";
+  })
+  pizzaClassic.addEventListener("mouseout", ()=> {
+    pizzaClassic.style.color = "white";
+  })
+  pizzaPopular.addEventListener("mouseenter", () => {
+    pizzaPopular.style.color = "red";
+  })
+  pizzaPopular.addEventListener("mouseout", ()=> {
+    pizzaPopular.style.color = "white";
+  })
+  pizzaNew.addEventListener("mouseenter", () => {
+    pizzaNew.style.color = "red";
+  })
+  pizzaNew.addEventListener("mouseout", ()=> {
+    pizzaNew.style.color = "white";
+  })
+}
